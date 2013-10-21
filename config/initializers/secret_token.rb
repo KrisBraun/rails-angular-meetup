a743c5e33ec003414a53c2cd627aaadc3da6c885
Kris Braun <kris@happenate.com>
2013-10-20 21:32:25 -0400

    Fresh Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAngularMeetup::Application.config.secret_token = 'a56efb3be40cd330e2ac2fc15e9069714230f1fc8f04c0aedfa77ef22fc7ca8b0b8f338ce9542bb894b4056fe21bcbe3dc7a6a2450d44e3c593a42c3d8b15b47'
